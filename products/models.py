8fff3d182ffc454c7fbbe6f38c94132400f0dc68
smeghen <smeghen@gmail.com>
2021-10-13 10:18:36 +0000

    Added Products app, models and manually added categroies and products using admin access

---
+++ products/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
