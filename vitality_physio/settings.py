91f9eee83fff6def7ef112836d2804da4119a7c6
smeghen <smeghen@gmail.com>
2021-10-20 13:51:35 +0000

    Add checkout views and template. Used crispy forms to create checkout form for entering customer details

--- vitality_physio/settings.py
+++ vitality_physio/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+    
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'vitality_physio.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -78,6 +83,10 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
