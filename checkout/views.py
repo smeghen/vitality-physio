f88fbe4c7fdf6084a80ab16d4d80a989391837cc
smeghen <smeghen@gmail.com>
2021-10-22 21:20:34 +0000

    Added checkout success logic which adds orders to database and stripe, with a confirmation message to the user

--- checkout/views.py
+++ checkout/views.py
@@ -49,8 +49,8 @@ def checkout(request):
                         order.delete()
                         return redirect(reverse('view_cart'))
 
-                request.session['save_info'] = 'save-info' in request.POST
-                return redirect(reverse('checkout_success', args=[order.order_number]))
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
 
         else:
             messages.error(request, 'There was an error with your form. \
