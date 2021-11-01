02445cd7d033004a61027890825c80ef4d66937f
smeghen <smeghen@gmail.com>
2021-11-01 15:41:50 +0000

    Updated models.py for contact app to make email a required field

--- contact/models.py
+++ contact/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Contact(models.Model):
     email = models.CharField(max_length=80,
-                             null=True, blank=True)
-    subject = models.CharField(max_length=255)
+                             null=True, blank=False)
+    subject = models.CharField(max_length=255,
+                             null=False, blank=False)
     message = models.CharField(max_length=1500, 
                              null=False, blank=False)
 
