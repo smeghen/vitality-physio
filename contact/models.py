4f2304830f916e4e396440d9dd7197744ecfb66d
smeghen <smeghen@gmail.com>
2021-11-02 16:35:24 +0000

    Updated Contact models.py to change the comment field into a text field so that it would be rendered accordingly

--- contact/models.py
+++ contact/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Contact(models.Model):
-    email = models.CharField(max_length=80,
+    email = models.EmailField(max_length=80,
                              null=True, blank=False)
     subject = models.CharField(max_length=255,
                               null=False, blank=False)
-    message = models.CharField(max_length=1500,
+    message = models.TextField(max_length=1500,
                                 null=False, blank=False)
 
     def __str__(self):
