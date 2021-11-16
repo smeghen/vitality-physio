e5cddaa01cf7bc842cabe9873df8eec51dc6da2f
smeghen <smeghen@gmail.com>
2021-11-16 18:59:58 +0000

    Fix for __str__(self) function for models.py for blogs and contacts as incorrect return self was initially used

--- blog/models.py
+++ blog/models.py
@@ -10,4 +10,4 @@ class Blog(models.Model):
                                 null=False, blank=False)
 
     def __str__(self):
-        return self.name
+        return self.subject
--- contact/models.py
+++ contact/models.py
@@ -10,4 +10,4 @@ class Contact(models.Model):
                                 null=False, blank=False)
 
     def __str__(self):
-        return self.name
+        return self.subject
