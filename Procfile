0dfe70ba93075ea05b10dd467d2e146ddd109bab
smeghen <smeghen@gmail.com>
2021-11-22 16:46:29 +0000

    Setting up Procfile and connection to Heroku for app deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vitality_physio.wsgi:application
--- vitality_physio/settings.py
+++ vitality_physio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-jz$9vr_49r+^lkp8lm5nsd*m1)vhnn%%!ohy*!zu%%r!$_!^1j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['smeghen-vitality-physio.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,17 @@ WSGI_APPLICATION = 'vitality_physio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
