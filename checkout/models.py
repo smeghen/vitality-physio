0834a8592fd1ee373ba288d92fb5e5e5bdc67f47
smeghen <smeghen@gmail.com>
2021-11-22 12:39:09 +0000

    Update checkout_success.html to display the paid amount for order when discpunt is applied

--- checkout/models.py
+++ checkout/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+from decimal import Decimal
 
 from products.models import Product
 from profiles.models import UserProfile
@@ -38,7 +39,7 @@ class Order(models.Model):
         Update grand total each time a line item is added
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
-
+        self.order_discount = self.order_total * Decimal(10/100)
         self.grand_total = self.order_total
         self.save()
 
